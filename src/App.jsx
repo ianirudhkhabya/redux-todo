b89bd95fa3d4acf830e14bd0511ecbf5c588bee6
anirudh <ianirudhkhabya@gmail.com>
2024-04-04 05:03:53 +0530

    Add TaskInput and TaskList components to App.jsx and wrap App component with Redux Provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
+import TaskInput from "./components/TaskInput";
+import TaskList from "./components/TaskList";
 
 function App() {
   return (
     <>
       <h1 className="text-2xl">Todo Application</h1>
+      <TaskInput />
+      <TaskList />
     </>
   );
 }
